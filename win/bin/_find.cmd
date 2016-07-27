f4d5922ec208aa8bc0b8a1ad294d83723dd24ca7
xiong-jia.le <lexiongjia@gmail.com>
2016-07-27 15:59:22 +0800

    update working settigns

--- win/bin/_find.cmd
+++ win/bin/_find.cmd
@@ -6,7 +6,6 @@ set _dev_env_libs=%~dp0.\..\lib
 call %_dev_env_libs%\settings.cmd
 
 set find_args=%*
-echo %_gnu_w32%
 %_gnu_w32%\bin\find.exe %find_args%
 endlocal
 
--- win/lib/_settings_wrk.cmd
+++ win/lib/_settings_wrk.cmd
@@ -4,21 +4,21 @@
 
 :: Proxy
 set _proxy_host=localhost
-set _proxy_port=8082
+set _proxy_port=8119
 
 :: dev tools
 set _git_home=C:\wrk\tools\git\Git-2.8.0
 set _gnu_w32=C:\wrk\tools\gnu_w32
 
 :: golang
-set _go_root=D:\usr\misc\go\go1.6.2
-set _go_path=E:\dev\gopath
+set _go_root=C:\wrk\tools\go\go1.6.2
+set _go_path=C:\wrk\gopath
 
 :: perl
 set _perl_home=C:\strawberry\perl
 
 :: node.js
-set _node=D:\usr\misc\node\node-v6.2.0
-set _npm=D:\usr\misc\node\npm-1.4.9
-set _npm_prefix=D:\usr\misc\node\npm-packages
+set _node=C:\wrk\tools\node\node-v4.4.5-64
+set _npm=C:\wrk\tools\node\npm-1.4.12
+set _npm_prefix=C:\wrk\tools\node\npm-prefix
 
