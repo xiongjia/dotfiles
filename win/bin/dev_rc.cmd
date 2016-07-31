2344d3ac87de9b1f17f63e02b5b723e6e18feb7f
xiongjia <lexiongjia@gmail.com>
2016-07-31 23:36:17 +0800

    update win script

--- win/bin/dev_rc.cmd
+++ win/bin/dev_rc.cmd
@@ -6,6 +6,7 @@ call %_dev_env_libs%\local_proxy.cmd
 call %_dev_env_libs%\dev_tools.cmd
 call %_dev_env_libs%\dev_tmp.cmd
 set path=%_dev_path%;%path%
+set prompt=[$p]$_(dev) $t $+$g
 
 call %_dev_env_libs%\dbg_info.cmd
 
