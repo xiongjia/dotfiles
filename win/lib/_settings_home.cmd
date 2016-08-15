25917c27172f1874ee5cb753df899ccbef057521
xiongjia <lexiongjia@gmail.com>
2016-08-15 21:39:36 +0800

    update curl folder

--- win/lib/_settings_home.cmd
+++ win/lib/_settings_home.cmd
@@ -7,6 +7,7 @@ set _tmp_root=F:\dev_tmp
 set _dropbox_root=E:\var\datum\Dropbox
 set _github_root=E:\dev\github
 set _chrome_root="C:\Program Files (x86)\Google\Chrome\Application"
+set _curl_root=D:\usr\misc\curl
 
 :: Proxy
 set _proxy_host=localhost
