a4ae066548b68fccff3c279f83236331a48c0002
xiongjia <lexiongjia@gmail.com>
2016-09-28 00:02:24 +0800

    add _yget

---
+++ win/bin/_yget.cmd
@@ -0,0 +1,27 @@
+@echo off
+
+setlocal
+set _dev_env_libs=%~dp0.\..\lib
+call %_dev_env_libs%\settings.cmd
+
+set _data_dir=%_storage_root%\youtube-dl
+set _ext_args=%*
+set _max_retry=2000
+set _yget_bin=%_dropbox_root%\tools\you-get\you-get.exe
+
+echo target:  %_data_dir%
+node -pe "`YGET Begin: ${(new Date()).toString()}`"
+
+set /a _tries=0
+:YGET_RETRY
+%_yget_bin% --no-proxy --output-dir %_data_dir% %_ext_args%
+set _yget_exit=%errorlevel%
+set /a _tries+=1
+node -pe "`YGET retry: ${(new Date()).toString()}`"
+sleep 10
+if %_tries% LEQ %_max_retry% ( goto YGET_RETRY ) else ( goto YGET_END )
+
+:YGET_END
+echo YGET End: tryies=%_tries%
+node -pe "`YGET End: ${(new Date()).toString()}`"
+endlocal
