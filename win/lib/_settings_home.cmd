3c06c6fea5e129c69e2b7454f290db7cbce69bc6
xiongjia <lexiongjia@gmail.com>
2017-01-30 13:57:42 +0800

    add gm

--- win/lib/_settings_home.cmd
+++ win/lib/_settings_home.cmd
@@ -85,6 +85,7 @@ set _gnuplot=D:\usr\misc\gnuplot\gp426win32\gnuplot
 set _ffmpeg=D:\usr\misc\ffmpeg\ffmpeg-20160828-a37e6dd-win64-static
 set _gpg_root=D:\usr\misc\gpg-win
 set _ledger_root=E:\var\datum\Dropbox\tools\ledger_win\ledger_3.1.1
+set _graphics_magick=D:\usr\misc\gm\GraphicsMagick-1.3.25-Q16
 
 :: Databases
 set _pg_root=E:\dev\pg\postgresql-9.6.1-1-x64
