df14ced9f520a97d9267f921df53359f5d4d639c
xiongjia <lexiongjia@gmail.com>
2018-10-14 14:37:38 +0800

    updating working settings

--- win/lib/_settings_wrk.cmd
+++ win/lib/_settings_wrk.cmd
@@ -3,90 +3,89 @@
 :: All settings (wrk)
 
 :: misc
-set _tmp_root=C:\wrk\env\dev_tmp
-set _storage_root=C:\wrk\env\dev_tmp
-set _dropbox_root=C:\Users\lexi\Dropbox
-set _onedrv_root=C:\Users\lexi\OneDrive
-set _github_root=C:\wrk\src\github
-set _3rd_src_root=C:\wrk\src
-set _chrome_root="C:\Program Files (x86)\Google\Chrome\Application"
-set _curl_root=C:\wrk\tools\curl\curl-7.33.0-win64-ssl-sspi
+set _tmp_root=D:\dat\cache\tmp
+set _storage_root=D:\dat\dev-tmp
+set _dropbox_root=
+set _onedrv_root=C:\Users\xiongjia_le\OneDrive
+set _github_root=D:\wrk\github
+set _3rd_src_root=D:\wrk\stuff
+set _chrome_root="C:\Users\xiongjia_le\AppData\Local\Google\Chrome\Application"
+set _curl_root=
 set _rsync_root=
 set _mc_root=
 
 :: Proxy
 set _proxy_host=localhost
-set _proxy_port=8119
+set _proxy_port=1095
 
 :: dev tools
-set _git_home=C:\wrk\tools\git\Git-2.8.0
-set _gnu_w32=C:\wrk\tools\gnu_w32
+set _git_home=C:\tools\git\git-2.19.1
+set _gnu_w32=C:\tools\gnu_w32
 
 :: golang
-set _go_root=C:\wrk\tools\go\go1.6.2
-set _go_path=C:\wrk\gopath
+set _go_root=
+set _go_path=
 
 :: perl
-set _perl_home=C:\strawberry\perl
+set _perl_home=
 
 :: node.js
-set _node=C:\wrk\tools\node\node-self\node-v8.9.2
-set _npm=C:\wrk\tools\node\node-self\npm-1.4.12
-set _npm_prefix=C:\wrk\tools\node\node-self\npm-prefix
-set NPM_CONFIG_USERCONFIG=C:\wrk\tools\node\node-self\npmrc
+set _node=C:\tools\node\node-v10.12.0
+set _npm=C:\tools\node\npm-v6.4.1
+set _npm_prefix=D:\dat\cache\npm_prefix
+set NPM_CONFIG_USERCONFIG=D:\dat\cache\npm_conf\npmrc
 
 :: c/c++
-set _boost_root=C:\wrk\src\boost\boost_1_61_0
-set _cmake_root=C:\wrk\tools\cmake\cmake-3.2.3
-set _ms_sym_cache=C:\wrk\env\sym
-set _ms_win_dbg_32=C:\wrk\tools\windbg\windbg_x86
-set _ms_win_dbg_64=C:\wrk\tools\windbg\windbg_x64
-set _ms_depends_32=C:\wrk\tools\depends\depends22_x86
-set _ms_depends_64=C:\wrk\tools\depends\depends22_x64
-set _llvm_root=C:\wrk\tools\LLVM
+set _boost_root=
+set _cmake_root=
+set _ms_sym_cache=
+set _ms_win_dbg_32=
+set _ms_win_dbg_64=
+set _ms_depends_32=
+set _ms_depends_64=
+set _llvm_root=
 
 :: python
-set _py2_home=C:\wrk\tools\py\py27
-set _py2_venv=C:\wrk\tools\py\py2venv
-
-set _py3_home=C:\wrk\tools\py\py36
-set _py3_venv=C:\wrk\tools\py\py3venv
+set _py2_home=
+set _py2_venv=
+set _py3_home=
+set _py3_venv=
 
 :: java
-set _jdk_home=C:\Program Files\Java\jdk1.8.0_51
-set _scala_home=C:\wrk\tools\scala\scala-2.12.0
-set _m2_home=C:\wrk\tools\maven\apache-maven-3.3.3
-set _gradle_home=C:\wrk\tools\gradle\gradle-2.4
-set _gradle_usr_home=C:\Users\lexi\.gradle
-set _sbt_home=C:\wrk\tools\scala\sbt-0.13.9
-set _ant_home=C:\wrk\tools\apache-ant\apache-ant-1.9.5
+set _jdk_home=C:\tools\jdk\jdk8.0.181
+set _scala_home=
+set _m2_home=
+set _gradle_home=
+set _gradle_usr_home=
+set _sbt_home=
+set _ant_home=
 
 :: ruby
-set _ruby_home=C:\tools\ruby21
-set _ruby_devkit=C:\tools\DevKit2
+set _ruby_home=
+set _ruby_devkit=
 
 :: vim
-set _gvim_root=C:\wrk\tools\vim\vim74
-set _gvim_home=C:\Users\lexi
-set _gvim_wrk_dir=C:\wrk\src
+set _gvim_root=C:\tools\vim\vim80
+set _gvim_home=D:\wrk\github\dotfiles\vim
+set _gvim_wrk_dir=D:\wrk
 
 :: emacs
-set _emacs_wrk_dir=C:\wrk\src
-set _emacs_root=C:\wrk\tools\emacs\emacs-25.0.91
+set _emacs_wrk_dir=
+set _emacs_root=
 set _emacs_bin=%_emacs_root%\bin\runemacs.exe
 set _emacs_home=C:\wrk\src\github\dotfiles\emacs
-set _emacs_https_proxy=localhost:8119
-set _emacs_http_proxy=localhost:8119
-set _emacs_tools=C:\Users\lexi\Dropbox\tools\emacs
+set _emacs_https_proxy=localhost:1095
+set _emacs_http_proxy=localhost:1095
+set _emacs_tools=
 
 :: tools
-set _putty_root=C:\wrk\tools\putty\putty-0.62
-set _choco_root=C:\ProgramData\chocolatey
-set _sys_internal=C:\wrk\tools\sysinternals
-set _platinum_searcher=C:\wrk\tools\pt
-set _gnu_global=C:\wrk\tools\global\glo653wb
-set _aspell=C:\wrk\tools\aspell
-set _gnuplot=C:\wrk\tools\gnuplot
+set _putty_root=C:\tools\putty\putty-0.70
+set _choco_root=
+set _sys_internal=C:\tools\sysinternals
+set _platinum_searcher=
+set _gnu_global=
+set _aspell=
+set _gnuplot=
 set _ffmpeg=
-set _gpg_root=C:\wrk\tools\gpg-win
+set _gpg_root=
 set _ledger_root=
