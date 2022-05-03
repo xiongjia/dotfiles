642875031c9fabd215c9ba8bd0ef9a52efc615bf
xiongjia <lexiongjia@gmail.com>
2022-05-03 23:16:47 +0800

    hello vim win build scripts

---
+++ tools/vim-windows-build-scripts/configure.cmd
@@ -0,0 +1,33 @@
+@echo off
+
+:: Windows SDK Include directory. No quotation marks.
+set SDK_INCLUDE_DIR=D:\app\Microsoft SDKs\Windows\v7.1\Include
+
+:: Visual Studio directory. Quotation marks.
+set VS_DIR=C:\Program Files (x86)\Microsoft Visual Studio\2017
+
+:: Target architecture, AMD64 (64-bit) or I386 (32-bit)
+set CPU=AMD64
+
+:: Toolchain, x86_amd64 (cross-compile 64-bit) or x86 (32-bit) or amd64 (64-bit)
+set TOOLCHAIN=x86_amd64
+
+:: TINY, SMALL, NORMAL, BIG or HUGE. NORMAL or above recommended
+set FEATURES=BIG
+`
+:: yes for gvim, no for vim
+set GUI=yes
+
+:: Whatever IDE integrations we don't need
+set NETBEANS=no
+
+:: UTF-8 encoding
+set MBYTE=yes
+
+:: Enable Python scripting
+set DYNAMIC_PYTHON=yes
+set PYTHON=D:\app\py\python27
+set PYTHON_VER=27
+
+echo "Configuring Visual Studio..."
+call "%VS_DIR%\Community\VC\Auxiliary\Build\vcvarsall.bat" %TOOLCHAIN%
