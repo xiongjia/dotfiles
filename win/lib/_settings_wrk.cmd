2b7bd34fc13cf32c1998101948ccbb01a3699d11
xiongjia <lexiongjia@gmail.com>
2022-05-03 21:46:43 +0800

    fix working folder config

--- win/lib/_settings_wrk.cmd
+++ win/lib/_settings_wrk.cmd
@@ -3,27 +3,29 @@
 :: All settings (wrk)
 
 :: misc
-set _tmp_root=D:\dat\cache\tmp
-set _storage_root=D:\dat\dev-tmp
+set _tmp_root=C:\wrk\cache\tmp
+set _storage_root=C:\wrk\data\dev-tmp
 set _dropbox_root=
 set _onedrv_root=C:\Users\xiongjia_le\OneDrive
-set _github_root=D:\wrk\github
-set _3rd_src_root=D:\wrk\stuff
-set _chrome_root="C:\Users\xiongjia_le\AppData\Local\Google\Chrome\Application"
-set _curl_root=C:\tools\curl\curl-7.61.1\bin
+set _github_root=C:\wrk\github
+set _3rd_src_root=C:\wrk\stuff
+set _chrome_root="C:\Program Files\Google\Chrome\Application"
+set _curl_root=C:\tools\curl\curl-7.79.1\bin
 set _rsync_root=
 set _mc_root=
+set HOME=C:\wrk\github\dotfiles\vim
+set USERPROFILE=C:\wrk\github\dotfiles\vim
 
 :: Proxy
 set _proxy_host=localhost
-set _proxy_port=1095
+set _proxy_port=8082
 set _no_proxy=localhost,127.0.0.1
 set _no_proxy=%_no_proxy%,registry.npm.taobao.org
 set _no_proxy=%_no_proxy%,cdn.npm.taobao.org,npm.taobao.org
 set _no_proxy=%_no_proxy%,github.com
 
 :: dev tools
-set _git_home=C:\tools\git\git-2.19.1
+set _git_home=C:\tools\git\git-2.33.1
 set _gnu_w32=C:\tools\gnu_w32
 
 :: golang
@@ -34,15 +36,15 @@ set _go_path=D:\dat\go-path
 set _perl_home=
 
 :: node.js
-set _node=C:\tools\node\node-v10.12.0
+set _node=C:\tools\node\node-v13
 set _npm=C:\tools\node\npm-v6.4.1
-set _npm_prefix=D:\dat\cache\npm_prefix
-set NPM_CONFIG_USERCONFIG=D:\dat\cache\npm_conf\npmrc
+set _npm_prefix=C:\tools\node\node_cache\npm_prefix
+set NPM_CONFIG_USERCONFIG=C:\tools\node\node_cache\npm_conf\npmrc
 
 :: c/c++
 set _boost_root=
 set _cmake_root=
-set _ms_sym_cache=D:\dat\cache\ms_sym
+set _ms_sym_cache=C:\wrk\data\ms_sym
 set _ms_win_dbg_32=C:\tools\windbg\windbg_x86
 set _ms_win_dbg_64=C:\tools\windbg\windbg_x64
 set _ms_depends_32=
@@ -56,11 +58,11 @@ set _py3_home=C:\tools\py\py3.7.0
 set _py3_venv=
 
 :: java
-set _jdk_home=C:\tools\jdk\jdk8.0.181
+set _jdk_home=C:\tools\java\jdk\jdk8.0.172
 set _scala_home=
-set _m2_home=C:\tools\maven\apache-maven-3.5.4
-set _gradle_home=C:\tools\gradle\gradle-4.10.2
-set _gradle_usr_home=D:\dat\cache\gradle
+set _m2_home=C:\tools\java\maven\apache-maven-3.8.3
+set _gradle_home=C:\tools\java\gradle\gradle-5.0
+set _gradle_usr_home=C:\tools\java\gradle\gradle_home
 set _sbt_home=
 set _ant_home=
 
@@ -70,8 +72,8 @@ set _ruby_devkit=
 
 :: vim
 set _gvim_root=C:\tools\vim\vim80
-set _gvim_home=D:\wrk\github\dotfiles\vim
-set _gvim_wrk_dir=D:\wrk
+set _gvim_home=c:\wrk\github\dotfiles\vim
+set _gvim_wrk_dir=c:\wrk
 
 :: emacs
 set _emacs_wrk_dir=
